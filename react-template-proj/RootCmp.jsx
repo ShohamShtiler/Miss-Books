db6a98d63d6094a707c32f0b5cc9bfe934774828
Shoham Shtiler <shtilershoham@gmail.com>
2025-02-15 11:21:10 +0200

    Add about-team and about-goal cmps

--- react-template-proj/RootCmp.jsx
+++ react-template-proj/RootCmp.jsx
@@ -1,22 +1,22 @@
-const { useState } = React
+
 const Router = ReactRouterDOM.HashRouter
 const { Routes, Route } = ReactRouterDOM
 
-import { AppHeader } from './cmps/AppHeader.jsx'
-
-import { AboutUs } from './pages/About.jsx'
-import { BookDetails } from './pages/BookDetails.jsx'
-import { BookIndex } from './pages/BookIndex.jsx'
 import { HomePage } from './pages/Home.jsx'
+import { AboutUs } from './pages/About.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
+
+
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { BookDetails } from './pages/BookDetails.jsx'
+
+
+import { AboutTeam } from './cmps/AboutTeam.jsx'
+import { AboutGoal } from './cmps/AboutGoal.jsx'
+
+
 
 export function App() {
-
-	// const [page, setPage] = useState('book')
-
-	// function onSetPage(page) {
-	// 	setPage(page)
-	// }
-
 	return (
 		<Router>
 		   <section className="app">
@@ -27,7 +27,11 @@ export function App() {
 			    <main>
 				<Routes>
 				  <Route path="/" element={<HomePage />} />
-				  <Route path="/about" element={<AboutUs />} />
+				  <Route path="/about" element={<AboutUs />} >
+				        <Route path='/about/Team' element={<AboutTeam />} />
+                        <Route path='/about/Goal' element={<AboutGoal />} />
+                    </Route>
+
 				  <Route path="/book" element={<BookIndex />} />
 				  <Route path="/book/:bookId" element={<BookDetails />} />
 				</Routes>
--- react-template-proj/pages/About.jsx
+++ react-template-proj/pages/About.jsx
@@ -1,11 +1,21 @@
+const { Link, Outlet } = ReactRouterDOM
+
 export function AboutUs(){
     return (
         <section className="about-us">
-            <span className="spaning">Hello!!!</span>
-            <span className="spaning">We are very interesting here</span>
-            <span className="spaning">We all love books!!</span>
-            <span className="spaning">We all love React!</span>
-            <span className="spaning">We all dont like Vue!</span>
+            <p>
+            Welcome to Miss Books, your go-to online bookstore for discovering and purchasing a wide selection of books.
+            Whether you're a passionate reader or just looking for your next great read, we provide a seamless and enjoyable shopping experience.
+            At Miss Books, we believe in the power of stories and knowledge, offering a carefully curated collection to suit all interests.
+            Browse, explore, and buy your favorite books with ease.
+            Happy reading! 📚✨
+            </p>
+            <nav>
+                <Link to='/about/Team' className="about-team"><i ></i> Team |</Link>
+                <Link to='/about/Goal' className="about-goal"><i ></i> Goal</Link>
+            </nav>
+            <Outlet />
+
         </section>
     )
 }
